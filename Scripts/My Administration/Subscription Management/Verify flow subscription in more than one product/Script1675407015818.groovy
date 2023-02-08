81b0bea2bbd1f0041713f778e8b9621828853d09
Thuy Truong (KO) <thuy.truong@katalon.com>
2023-02-08 10:20:12 +0700

    commit test cases one more prod

---
+++ Scripts/My Administration/Subscription Management/Verify flow subscription in more than one product/Script1675407015818.groovy
@@ -0,0 +1,17 @@
+import com.kms.katalon.util.CryptoUtil
+
+import internal.GlobalVariable
+import katalon.fw.lib.Page
+import katalon.services.SubscriptionService
+import katalon.testops.services.LoginService
+
+String decryptedPwd =  CryptoUtil.decode(CryptoUtil.getDefault(password))
+
+'User login to get token '
+Page.nav(LoginService).login(email, decryptedPwd).verifyStatusCode(200).getToken()
+
+Page.nav(SubscriptionService)
+							.createNewSubscription(accountId, planId, number)
+							.upgradeSubscription(accountId, planId, number)
+							.cancelSubscription()
+							.reactivateSubscription()
