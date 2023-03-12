8b7f9853005651c858b56b09c17185d10ea597cc
Thuy Truong 93 <thuy.truong@katalon.com>
2023-03-12 19:36:22 +0700

    add table into testcases

--- Keywords/katalon/db/katone/BillingStudioTable.groovy
+++ Keywords/katalon/db/katone/BillingStudioTable.groovy
@@ -6,7 +6,16 @@ import katalon.fw.lib.Page
 
 public class BillingStudioTable extends BaseTable<BillingStudioTable> {
 
+	private BillingStudioTable() {
+		dbName = "katone"
+		table = "billing.studio_subscriptions"
+	}
+	
 	public BillingStudioTable selectStudioSubscription(Number accountId) {
+		def selectScript = "select * from billing.studio_subscriptions ats2 where ats2.accountid  = $accountId and ats2.status = 'ACTIVE';"
 
+		def values = execute(selectScript)
+		println "value: $values"
+		return this
 	}
 }
