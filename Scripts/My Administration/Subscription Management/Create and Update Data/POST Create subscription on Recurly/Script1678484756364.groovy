b209814aebc8d3b07396435cc6e6600eb0d369ed
Thuy Truong (KO) <thuy.truong@katalon.com>
2023-03-14 11:49:37 +0700

    add data renew

--- Scripts/My Administration/Subscription Management/Create and Update Data/POST Create subscription on Recurly/Script1678484756364.groovy
+++ Scripts/My Administration/Subscription Management/Create and Update Data/POST Create subscription on Recurly/Script1678484756364.groovy
@@ -10,5 +10,5 @@ Page.nav(SubscriptionService).getListActiveSubscription(accountId)
 Page.nav(SubscriptionService).createNewSubscriptionRecurly(accountId, planId, quantity)
 
 						 
-//Page.nav(SubscriptionService).updateNextBillingDate(next_bill_date, subscriptionBody.uuid)
-//							 .markPaidInvoiceSubscriptionByInvoiceId(subscriptionBody.active_invoice_id)
+Page.nav(SubscriptionService).updateNextBillingDate(next_bill_date, subscriptionBody.uuid)
+							 .markPaidInvoiceSubscriptionByInvoiceId(subscriptionBody.active_invoice_id)
